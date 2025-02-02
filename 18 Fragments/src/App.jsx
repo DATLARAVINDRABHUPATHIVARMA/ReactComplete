952a915e8d35d2ec49c768be80c09eec16aefb48
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-02-01 22:30:21 -0600

    React

--- 18 Fragments/src/App.jsx
+++ 18 Fragments/src/App.jsx
@@ -1,4 +1,5 @@
 import "./app.css"
+import Container from "./components/Container"
 import ErrorMessage from './components/ErrorMessage'
 import Warriors from './components/Warriors'
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -7,11 +8,11 @@ function App() {
  let warriors = ["Maharathi Radheya Ravindra", "Highness Yuvarani Sruthi", "Srujan", "Yok Shit", "Sanvika pit"]
  
   return (
-    <>
+    <Container>
       <h1 className="titled">Strong Warriors of the Empire</h1>
       <ErrorMessage wars={warriors}></ErrorMessage>
       <Warriors wars={warriors}></Warriors>
-    </>
+    </Container>
   )
 }
 
--- 18 Fragments/src/components/Item.jsx
+++ 18 Fragments/src/components/Item.jsx
@@ -1,5 +1,6 @@
 const Item = ({ warrior }) => {
-  return <li className="list-group-item highlight">{warrior}</li>;
+  return <li className="list-group-item highlight">
+    <span className="bit">{warrior}</span></li>;
 };
 
 export default Item;
