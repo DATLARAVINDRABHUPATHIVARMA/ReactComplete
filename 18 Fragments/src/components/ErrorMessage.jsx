d95ac352c6c1650860c51db2a2bda45eca81d912
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-03-10 23:29:14 -0600

    React

--- 18 Fragments/src/components/ErrorMessage.jsx
+++ 18 Fragments/src/components/ErrorMessage.jsx
@@ -1,5 +1,5 @@
 const ErrorMessage = ({wars}) => {
     return (<>{wars.length === 0 ? <h3>In Highness Yuvarani Sruthi's Empire Maharathi Ravindra is General</h3> : null }</>)
 }
-dsfvcascgv vdcn
+dsfvcascgv
 export default ErrorMessage;
