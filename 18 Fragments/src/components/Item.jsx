1207cbc2ab7b0fa12844a098ba4f29030ca8a3c0
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-04-12 22:34:39 -0500

    React

--- 18 Fragments/src/components/Item.jsx
+++ 18 Fragments/src/components/Item.jsx
@@ -4,4 +4,4 @@ const Item = ({ warrior }) => {
 };
 
 export default Item; 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deleniti, commodi iusto consectetur corrupti obcaecati.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deleniti, commodi iusto consectetur corrupti
