6aac61dff2df1a804af7a6cd73cef079fdd6f451
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-08-09 06:43:03 -0500

    Work well

--- 18 Fragments/src/components/Item.jsx
+++ 18 Fragments/src/components/Item.jsx
@@ -1,5 +1,5 @@
-const Item = ({warrior}) => {
-    return <li className='list-group-item highlight'>{warrior}</li>
-}
+const Item = ({ warrior }) => {
+  return <li className="list-group-item highlight">{warrior}</li>;
+};
 
 export default Item;
