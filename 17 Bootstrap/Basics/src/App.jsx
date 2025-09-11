5e656618ae6f01bae118800f3e16d53369ac6a1b
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-09-11 13:38:46 +0530

    gained some better understanding of bootstrap

---
+++ 17 Bootstrap/Basics/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <button type="button" class="btn btn-primary">Sruthi The Highness Main Head of the Clan</button>
+      <button type="button" class="btn btn-secondary">Sravani The Princess</button>
+      <button type="button" class="btn btn-success">Suneel The King</button>
+      <button type="button" class="btn btn-danger">Ravindra The Protector and Commander-in-Chief of dynasty </button>
+      <button type="button" class="btn btn-warning">Radha The Queen</button>
+      <button type="button" class="btn btn-info">Rangavathi Queen Mother</button>
+      <button type="button" class="btn btn-light">Leo The Guard</button>
+      <button type="button" class="btn btn-dark">Suryavathi </button>
+      <button type="button" class="btn btn-link">Trisha</button>
+    </div> 
+  )
+}
+
+export default App
