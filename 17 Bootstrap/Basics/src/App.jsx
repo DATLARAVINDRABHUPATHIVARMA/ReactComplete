793837b01a3adc6e9eceede89cd65f53bbe13f65
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-11-24 16:15:49 +0530

    Work Schedule

--- 17 Bootstrap/Basics/src/App.jsx
+++ 17 Bootstrap/Basics/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <button type="button" class="btn btn-success">Suneel The King</button>
       <button type="button" class="btn btn-danger">Ravindra The Protector and Commander-in-Chief of dynasty </button>
       <button type="button" class="btn btn-warning">Radha The Queen</button>
-      <button type="button" class="btn btn-info">Rangavathi Queen Mother</button>
+      <button type="button" class="btn btn-info">Rangavathi</button>
       <button type="button" class="btn btn-light">Leo The Guard</button>
       <button type="button" class="btn btn-dark">Suryavathi The Grand Warden</button>
       <button type="button" class="btn btn-link">Trisha The Minister</button>
