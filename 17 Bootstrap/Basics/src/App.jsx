380042aaa452a73c262d1a60d40c4b3c2e2a9034
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-11-29 22:33:29 -0600

    Tamm

--- 17 Bootstrap/Basics/src/App.jsx
+++ 17 Bootstrap/Basics/src/App.jsx
@@ -4,11 +4,11 @@ function App() {
     <div class="m-2">
       <button type="button" class="m-2 btn btn-primary">Sruthi The Highness Emperor</button>
       <button type="button" class="m-2 btn btn-secondary">Sravani The Princess</button>
-      <button type="button" class="m-2 btn btn-success">Suneel The King</button>
+      <button type="button" class="m-2 btn btn-success">Suneel The Wise King</button>
       <button type="button" class="m-2 btn btn-warning">Radha The Queen</button>
-      <button type="button" class="m-2 btn btn-danger">Ravindra The Protector and Commander-in-Chief of dynasty </button>
-    </div> 
-  )
+      <button type="button" class="m-2 btn btn-danger">Ravindra The loyal Protector and Commander-in-Chief of dynasty </button> 
+      <button type="button" class="m-2 btn btn-info">Rangavathi The Great Wise Queen Mother</button>
+    </div>)
 }
 
 export default App
