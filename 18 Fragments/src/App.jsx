63f274445205df4b52ecbec8f280a084ea9ae700
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-12-05 04:00:40 -0600

    Updates with Map method

--- 18 Fragments/src/App.jsx
+++ 18 Fragments/src/App.jsx
@@ -1,8 +1,16 @@
+import './App.css'
+import React from 'react' 
 
 function App() {
+ let warriors = ["Maharathi Radheya Ravindra", "Highness Yuvarani Sruthi", "Srujan", "Yok Shit", "Sanvika pit"]
  
   return (
     <>
+      <h1>Strong Warriors of the Empire</h1>
+      {warriors.length >= 3 ? <h3>In Highness Yuvarani Sruthi's Empire Maharathi Ravindra is General</h3> : null }
+      <ul className="list-group">
+        {warriors.map((warrior) => (<li key={warrior} className='list-group-item'>{warrior}</li>))}
+      </ul>
     </>
   )
 }
