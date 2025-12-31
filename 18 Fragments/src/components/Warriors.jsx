4fc0704e212aea654a408b6430c22edde0b76841
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-12-31 10:21:50 +0530

    Create

--- 18 Fragments/src/components/Warriors.jsx
+++ 18 Fragments/src/components/Warriors.jsx
@@ -1,12 +1,13 @@
 import Item from "./Item";
 
-const Warriors = ({wars}) => {
-    
-    return (
+const Warriors = ({ wars }) => {
+  return (
     <ul className="list-group">
-        {wars.map((war) => (<Item key={war} warrior={war}></Item>))}
+      {wars.map((war) => (
+        <Item key={war} warrior={war}></Item>
+      ))}
     </ul>
-    );
-}
+  );
+};
 
 export default Warriors;
