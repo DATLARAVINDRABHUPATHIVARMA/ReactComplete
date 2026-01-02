51b837bd8417a485dfd9704456e492aaeec7464e
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-01-03 02:52:39 +1400

    Work

--- 18 Fragments/src/components/Warriors.jsx
+++ 18 Fragments/src/components/Warriors.jsx
@@ -4,4 +4,4 @@ const Warriors = ({ wars }) => {
   return (<ul className="list-group">{wars.map((war) => (<Item key={war} warrior={war}></Item>))}</ul>);
 };
 
-export default Warriors;
+export default Warriors; 
