1d912f27b3b0d4276ba784d068f78b82f6d73a11
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2026-04-01 03:10:40 -0500

    Dropdown with sidebar

--- 18 Fragments/src/components/Warriors.jsx
+++ 18 Fragments/src/components/Warriors.jsx
@@ -1,13 +1,7 @@
 import Item from "./Item";
 
 const Warriors = ({ wars }) => {
-  return (
-    <ul className="list-group">
-      {wars.map((war) => (
-        <Item key={war} warrior={war}></Item>
-      ))}
-    </ul>
-  );
+  return (<ul className="list-group">{wars.map((war) => (<Item key={war} warrior={war}></Item>))}</ul>);
 };
 
 export default Warriors;
